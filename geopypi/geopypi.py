54aa1b0ee3dd5e3af64863031e7684982650341f
Metaliguns77 <kshakibu@vols.utk.edu>
2024-03-04 14:40:04 -0500

    small changes

--- geopypi/geopypi.py
+++ geopypi/geopypi.py
@@ -1,1 +1,8 @@
 """Main module."""
+
+import ipyleaflet
+
+class Map(ipyleaflet.Map):
+    def __init__(self, center= [20,0], **kwargs):
+        super().__init__(center=center, **kwargs)
+        self.add_control(ipyleaflet.LayersControl())
